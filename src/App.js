6de34ba7695cafdede392b3aabd2af4597978898
Luke Campagnola <luke.campagnola@gmail.com>
2020-12-28 15:46:55 -0800

    midi key press/release working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
+import { useState, useEffect } from 'react'
+import { update } from 'ramda'
 import { PianoOverlay } from './components/piano_overlay'
 import './App.css';
+window.R = require('ramda')
 
 function App() {
+  let inputs, outputs
+  const [pressedKeys, setPressedKeys] = useState(new Array(88).fill(false))
+
+  useEffect(() => {
+    const onMIDISuccess = (midiAccess) => {
+      console.log(midiAccess)
+
+      inputs = midiAccess.inputs
+      outputs = midiAccess.outputs
+
+      for (var input of midiAccess.inputs.values()) {
+        input.onmidimessage = getMIDIMessage
+      }
+    }
+
+    const onMIDIFailure = () => {
+      console.log('Could not access your MIDI devices.')
+    }
+
+    const getMIDIMessage = (midiMessage) => {
+      switch( midiMessage.data[0] ) {      
+        case 144: {  // note on
+          const keyId = midiMessage.data[1] - 21
+          const newPressedKeys = update(keyId, true, pressedKeys)
+          console.log("new pressed keyts:", newPressedKeys)
+          setPressedKeys(newPressedKeys)
+          console.log(keyId)
+          break
+        }
+        case 128: { // note off
+          const keyId = midiMessage.data[1] - 21
+          setPressedKeys(update(keyId, false, pressedKeys))
+          // console.log(keyId)
+          break
+        }
+      }
+    }
+
+    navigator.requestMIDIAccess()
+      .then(onMIDISuccess, onMIDIFailure)
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
           noop?
         </p>
-        <PianoOverlay/>
+        <PianoOverlay pressedKeys={pressedKeys}/>
       </header>
     </div>
   );
--- src/components/piano_overlay.js
+++ src/components/piano_overlay.js
@@ -2,20 +2,22 @@ import React from 'react';
 import { times } from 'ramda';
 import './piano_overlay.scss';
 
-const PianoKey = ({keyId}) => {
+const PianoKey = ({keyId, pressed}) => {
     const isBlackKey = [1, 4, 6, 9, 11].includes(keyId % 12)
     const keyClass = isBlackKey ? "PianoKey-black" : "PianoKey-white"
+    const xPos = isBlackKey ? keyId*1.136 : keyId*1.923
+    const pressClass = pressed ? "PianoKey-pressed" : "PianoKey-unpressed"
+    console.log("PianoKey", keyId, pressed)
     return (
-        <div class={`PianoKey ${keyClass}`} style={{left: `${keyId*1.136}%`}}>{keyId}</div>
+        <div className={`PianoKey ${keyClass} ${pressClass}`} style={{left: `${xPos}%`}}></div>
     )
 }
 
-export const PianoOverlay = (props) => {
-
+export const PianoOverlay = ({pressedKeys}) => {
+    console.log("PianoOverlay", pressedKeys)
     return (
-        <div class='PianoOverlay'>
-            Piano overlay
-            { times(i => <PianoKey keyId={i}></PianoKey>, 88) }
+        <div className='PianoOverlay'>
+            { times(i => <PianoKey keyId={i} key={i} pressed={pressedKeys[i]}></PianoKey>, 88) }
         </div>
     )
 }
